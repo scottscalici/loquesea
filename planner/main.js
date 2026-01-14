7a044051e26c375957dfa59445e24900e3890fee
scottscalici <scott.scalici@gmail.com>
2026-01-13 23:08:39 -0500

    Update main.js

--- planner/main.js
+++ planner/main.js
@@ -3,7 +3,7 @@ async function buildMyDay() {
         // 1. PATHS TO YOUR JSON DATA
         const scheduleUrl = 'https://raw.githubusercontent.com/scottscalici/loquesea/main/planner/schedule.json';
         const calendarUrl = 'https://raw.githubusercontent.com/scottscalici/imagenes/main/planes/calendario.json';
-        const coziUrl = 'https://corsproxy.io/?' + encodeURIComponent('PASTE_YOUR_COZI_ICS_LINK_HERE');
+        const coziUrl = 'https://corsproxy.io/?' + encodeURIComponent('https://rest.cozi.com/api/ext/1103/f9f7020d-05c9-4720-b813-2155b4485be7/icalendar/feed/feed.ics');
 
         // 2. FETCH ALL DATA SIMULTANEOUSLY
         const [scheduleRes, calendarRes, coziRes] = await Promise.all([
